adb4493939c2f974eab20ff3312e18b43a3fb9fe
generalkatony <generalkatony@gmail.com>
2023-10-23 14:59:35 +0100

    csv read and upload capable script

--- future_work/Molecule_inject_GUI.py
+++ future_work/Molecule_inject_GUI.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
 import json
-
+from icecream import ic
 
 ############## GENERAL FUNCTIONS ############################
 
@@ -48,6 +48,7 @@ token_entry = ttk.Entry(info_frame)
 token_entry.grid(row=2, column=1)
 
 
+##############################
 def set_path(entry_field):
     path = filedialog.askopenfilename()
     entry_field.delete(0, tk.END)
@@ -63,10 +64,21 @@ btn_get_path = ttk.Button(
     info_frame, text="Select File", command=lambda: set_path(csv_entry)
 )
 btn_get_path.grid(row=1, column=4)
-########### UNIVERSAL DATA DICT ############################
+
+###### INFO DICT ####################################
+
+info_value = {}
+
+###### GET N STORE VALUES ###########################
+
+
+########### UNIVERSAL FIELD DATA DICT ######################
+
 entry_widgets = {}
 entered_value = []  # list for entered values
-############################################################
+
+############# DEFAULT FIELD FRAME ##########################
+
 # Fields LabelFrame
 fields_frame = ttk.LabelFrame(frame, text="Default Fields")
 fields_frame.grid(row=1, column=0, padx=10, pady=10)
@@ -87,7 +99,7 @@ variables = [
     "custom_field_name",
 ]
 
-
+####### CREATE VALUE INPUT FRAME #####################
 for i, var_name in enumerate(variables):
     label = ttk.Label(fields_frame, text=var_name.capitalize() + ":")
     label.grid(row=i, column=0, sticky="ew")
@@ -99,31 +111,8 @@ for i, var_name in enumerate(variables):
     entry_widgets[var_name] = entry
 
 
-def get_values():
-    for var_name, entry_widget in entry_widgets.items():
-        value = entry_widget.get()
-        if value is not None and value != "":
-            entered_value.append({var_name, value})
+################# dark/ light mode #########################
 
-    ########## PRINT WITH FORMAT ##################################
-
-    formatted_value = []
-    # Format the values and append to the formatted_values list
-    for var_name, value in entered_value:
-        formatted_value.append(f"{var_name.capitalize()}: {value}")
-
-    # Now, you can print the formatted values
-    print("Formatted Entered Values:")
-    for formatted_value in formatted_value:
-        print(formatted_value)
-
-
-###############################################################
-
-get_values_button = ttk.Button(fields_frame, text="Get Values", command=get_values)
-get_values_button.grid(row=len(variables), columnspan=2)
-
-############################################################
 # add dark/light mode toggle
 mode_switch = ttk.Checkbutton(frame, text="Mode", style="Switch", command=toggle_mode)
 mode_switch.grid(row=6, column=0, padx=5, pady=10, sticky="nsew")
@@ -138,28 +127,75 @@ cus_entry_widgets = {}
 
 
 def add_entry():
-    label_text = f"Custom Field {len(cus_entry_widgets) + 1}"
     custom_label = ttk.Entry(cusfields_frame)
     custom_label.grid(row=len(cus_entry_widgets), column=0)
     custom_value = ttk.Entry(cusfields_frame)
     custom_value.grid(row=len(cus_entry_widgets), column=1)
     # Store the new entry widgets with descriptive keys
-    cus_entry_widgets[label_text] = (custom_label, custom_value)
-
-    # Print the values of each entry box, if not empty
-    print("Values of Custom Fields:")
-    for label, (entry1, entry2) in cus_entry_widgets.items():
-        value1 = entry1.get()
-        value2 = entry2.get()
-
-
-# Iterate and print each key-value pair in the cus_entry_widgets dictionary
-for label, (entry1, entry2) in cus_entry_widgets.items():
-    print(f"{label}: Label - {entry1.get()}, Value - {entry2.get()}")
+    cus_entry_widgets.update({custom_label: custom_value})
+    # ic(cus_entry_widgets)
 
 
 add_button = ttk.Button(cusfields_frame, text="Add Entry", command=add_entry)
 add_button.grid(row=15, column=0)
+
+
+####### GET VALUES FROM BUTTON ###################
+def get_values(w, entered_values):
+    for var_name, entry_widget in w.items():
+        value = entry_widget.get()
+        if value is not None and value != "":
+            entered_values.append({var_name: value})
+
+    formatted_values = []  # Define the list for formatted values
+
+    # Format the values and append to the formatted_values list
+    for entry in entered_values:
+        for var_name, value in entry.items():
+            formatted_values.append(f"{var_name.capitalize()}: {value}")
+
+    # Now, you can print the formatted values
+    print("Formatted Entered Values:")
+    for formatted_value in formatted_values:
+        ic(formatted_value)
+
+
+def get_custom_values(w, entered_values):
+    for cus_label, cus_value in w.items():
+        label = cus_label.get()
+        value = cus_value.get()
+
+        if label is not None and label != "":
+            if value is not None and value != "":
+                ic(entered_values.append({label: value}))
+
+    formatted_values = []  # Define the list for formatted values
+
+    # Format the values and append to the formatted_values list
+    for entry in entered_values:
+        for cus_text, label in entry.items():
+            formatted_values.append(f"{cus_text.capitalize()}: {label}")
+
+    # Now, you can print the formatted values
+    print("Formatted Entered Values:")
+    for formatted_value in formatted_values:
+        print(formatted_value)
+
+
+############### CREATE GET VALUE BUTTON ####################
+
+
+def get_all_values():
+    get_values(entry_widgets, entered_values=entered_value)
+    get_custom_values(cus_entry_widgets, entered_values=entered_value)
+
+
+get_values_button = ttk.Button(
+    fields_frame,
+    text="Get Values",
+    command=lambda: get_all_values(),
+)
+get_values_button.grid(row=len(variables), columnspan=2)
 
 #############################################################
 
--- load_n_link.py
+++ load_n_link.py
@@ -7,11 +7,7 @@ from openpyxl import load_workbook
 
 ###################################################
 # url and dummy_test_input here
-trade_id = 149198
 
-get_subleg_url = "https://ct-eu.molecule.io/api/v2/legs/sublegs?trade_id=" + str(
-    trade_id
-)
 
 ticket_url = "https://ct-eu.molecule.io/api/v2/inventory/tickets"
 
@@ -26,6 +22,9 @@ df_data = pd.read_csv(
     r"Example_data.csv",
     header=0,
 )
+
+# Specify the CSV file path for SUBLEG ID FETCH DATA
+subleg_file_path = "response_data.csv"
 
 df_data = df_data.fillna("")
 
@@ -45,7 +44,7 @@ input3 = "volume"
 input4 = "price"
 input5 = "asset"
 input6 = "status"
-input7 = "fill"
+# input7 = "fill"
 input8 = "final_delivery"
 # input9 = str(subleg_id)
 input10 = "external_id"
@@ -53,125 +52,152 @@ input11 = "external_source"
 input12 = "dedupe_external_id"
 input13 = "custom_field_name"
 
-
-################ SUBMIT API REQUEST TO FETCH SUBLEG ID ###################################
-
-# API requests for subleg ID of the given trade ID
-response = requests.get(get_subleg_url, headers=headers)
-
-# Parse the JSON response
-response_data = response.json()
-
-# print response for checking
-print(json.dumps(response_data, separators=(",", ":"), indent=4))
-
-
-############# filter n panda transform retrived subleg data ##################
-
-# Extract data from the JSON response
-res_data = response_data["data"]
-
-# Create an empty list to store the flattened data (unnest all the data inside atrributes)
-flattened_data = []
-
-# Iterate through the data and flatten the attributes
-for item in res_data:
-    record = {"id": item["id"], "type": item["type"], **item["attributes"]}
-    flattened_data.append(record)
-
-# Create a Pandas DataFrame from the flattened data
-df = pd.DataFrame(flattened_data)
-
-
-print(df)
-
-########### for writing logs to excel file ##################################
-
-# log_loc = "response_data.xlsx"
-
-# Create a new Excel writer
-# writer = pd.ExcelWriter(log_loc, engine="openpyxl")
-
-# Write the DataFrame to the Excel file
-# df.to_excel(writer, sheet_name="Data", index=False)
-
-# Save the Excel file using writer.book.save()
-# writer.book.save(log_loc)
-
-############ for writing the subleg_id data to csv format#############################################
-
-# Specify the CSV file path
-csv_file_path = "response_data.csv"
-
-# Write the DataFrame to a CSV file
-df.to_csv(csv_file_path, index=True)
-
-############## THIS is the READ SUBLEG_ID N UPLOAD section##############################
-
-# Read the saved response
-df = pd.read_csv(
-    r"response_data.csv",
-    header=0,
-)
-
-# Select which subleg to write to [molecule returns as 'ID']
-selected_subleg = 0
-subleg_id = int(df["id"].iloc[selected_subleg])
-
-# Load n Populate the Payload Draft
 for idx, data in df_data.iterrows():
-    payload = {
-        "fulfillment_date": data[input1],
-        "commodity": data[input2],
-        "volume": data[input3],
-        "price": data[input4],
-        "asset": data[input5],
-        "status": data[input6],
-        "fill": data[input7],
-        "final_delivery": data[input8],
-        "subleg_id": str(subleg_id),
-        "external _id": data[input10],
-        "external_source": data[input11],
-        "dedupe_external_id": data[input12],
-        "custom_field_name": data[input13],
+    ################ SUBMIT API REQUEST TO FETCH SUBLEG ID ###################################
+
+    trade_id = []
+    subleg_id = []
+
+    if data["trade_id"] is not None and data["trade_id"] != "":
+        trade_id = data["trade_id"]
+
+        get_subleg_url = (
+            "https://ct-eu.molecule.io/api/v2/legs/sublegs?trade_id=" + str(trade_id)
+        )
+
+        # API requests for subleg ID of the given trade ID
+        response = requests.get(get_subleg_url, headers=headers)
+
+        # Parse the JSON response
+        response_data = response.json()
+
+        # print response for checking
+        # print(json.dumps(response_data, separators=(",", ":"), indent=4))
+
+        ############# filter n panda transform retrived subleg data ##################
+
+        # Extract data from the JSON response
+        res_data = response_data["data"]
+
+        # Create an empty list to store the flattened data (unnest all the data inside atrributes)
+        flattened_data = []
+
+        # Iterate through the data and flatten the attributes
+        for item in res_data:
+            record = {"id": item["id"], "type": item["type"], **item["attributes"]}
+            flattened_data.append(record)
+
+        # Create a Pandas DataFrame from the flattened data
+        df = pd.DataFrame(flattened_data)
+
+        print(df)
+
+        ########### for writing logs to excel file ##################################
+
+        # log_loc = "response_data.xlsx"
+
+        # Create a new Excel writer
+        # writer = pd.ExcelWriter(log_loc, engine="openpyxl")
+
+        # Write the DataFrame to the Excel file
+        # df.to_excel(writer, sheet_name="Data", index=False)
+
+        # Save the Excel file using writer.book.save()
+        # writer.book.save(log_loc)
+
+        ############ for writing the subleg_id data to csv format#############################################
+
+        # Write the DataFrame to a CSV file
+        df.to_csv(subleg_file_path, index=True)
+
+        ############## THIS is the READ SUBLEG_ID N UPLOAD section##############################
+
+        # Read the saved response
+        df = pd.read_csv(
+            r"response_data.csv",
+            header=0,
+        )
+
+        # Select which subleg to write to [molecule returns as 'ID']
+        selected_subleg = 0
+        subleg_id = int(df["id"].iloc[selected_subleg])
+
+    else:
+        trade_id = ""
+        subleg_id = ""
+
+    # Load n Populate the Payload Draft
+
+    if str(subleg_id) is not None and str(subleg_id) != "":
+        payload = {
+            "fulfillment_date": data[input1],
+            "commodity": data[input2],
+            "volume": data[input3],
+            "price": data[input4],
+            "asset": data[input5],
+            "status": data[input6],
+            "fill": "true",
+            "final_delivery": data[input8],
+            "subleg_id": str(subleg_id),
+            "external _id": data[input10],
+            "external_source": data[input11],
+            "dedupe_external_id": data[input12],
+            "custom_field_name": data[input13],
+        }
+    else:
+        payload = {
+            "fulfillment_date": data[input1],
+            "commodity": data[input2],
+            "volume": data[input3],
+            "price": data[input4],
+            "asset": data[input5],
+            "status": data[input6],
+            "fill": "false",
+            "final_delivery": data[input8],
+            "external _id": data[input10],
+            "external_source": data[input11],
+            "dedupe_external_id": data[input12],
+            "custom_field_name": data[input13],
+        }
+    ####################### FILTER DATA SECTION ###########################################
+
+    ######### remove n/a data from package ########################
+
+    # Define values to remove
+    values_to_remove = {"", "n/a", "N/a", "N/A", "n/A", "empty"}
+
+    # filter the payload
+    filtered_payload = {
+        key: value
+        for key, value in payload.items()
+        if value is not None and value not in values_to_remove
     }
-####################### FILTER DATA SECTION ###########################################
 
-######### remove n/a data from package ########################
+    ######## check if CHECKBOX condition is valid##################
 
-# Define values to remove
-values_to_remove = {"", "n/a", "N/a", "N/A", "n/A", "empty"}
+    allowed_statuses = {
+        "status": {"adjustment", "estimate", "in_transit", "delivered/received"},
+        "fill": {"true", "false"},
+        "final_delivery": {"true", "false"},
+        "dedupe_external_id": {"true", "false"},
+    }
+    # Define the allowed statuses
 
-# filter the payload
-filtered_payload = {
-    key: value
-    for key, value in payload.items()
-    if value is not None and value not in values_to_remove
-}
+    for key, value in allowed_statuses.items():
+        if key in filtered_payload and filtered_payload[key] not in value:
+            del filtered_payload[key]
 
-######## check if CHECKBOX condition is valid##################
+    ############### UPLOAD DATA N PRINT RESPONSE ###########################################
 
-allowed_statuses = {
-    "status": {"adjustment", "estimate", "in_transit", "delivered/received"},
-    "fill": {"true", "false"},
-    "final_delivery": {"true", "false"},
-    "dedupe_external_id": {"true", "false"},
-}
-# Define the allowed statuses
+    # Send the JSON data in the request
+    response = requests.post(
+        ticket_url, data=json.dumps(filtered_payload), headers=headers
+    )
+    print("\n sent package looks like " + json.dumps(filtered_payload))
 
-for key, value in allowed_statuses.items():
-    if key in filtered_payload and filtered_payload[key] not in value:
-        del filtered_payload[key]
-
-############### UPLOAD DATA N PRINT RESPONSE ###########################################
-
-# Send the JSON data in the request
-response = requests.post(ticket_url, data=json.dumps(filtered_payload), headers=headers)
-print("\n sent package looks like " + json.dumps(filtered_payload))
-
-# print(f"\n Request failed with status code: {response.status_code}")
-print(f"\n Status code: {response.status_code} ( {response.reason} )\n")
-print(
-    "Ticket loaded as: \n\n"
-    + json.dumps(response.json(), separators=(",", ":"), indent=4)
-)
+    # print(f"\n Request failed with status code: {response.status_code}")
+    print(f"\n Status code: {response.status_code} ( {response.reason} )\n")
+    print(
+        "Ticket loaded as: \n\n"
+        + json.dumps(response.json(), separators=(",", ":"), indent=4)
+    )
